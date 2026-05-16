b0236a837ef05fa5f648aed77bc74341d8a949a5
Housinsectsoffperu <housinsectsoffperu@192.168.18.23>
2026-05-16 09:20:43 -0500

    Fix build error

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,7 @@
 'use client'
-
-import { NextStudio } from 'sanity/next-studio'
-import config from '@/../sanity.config'
-
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../../sanity.config'
+export const dynamic = 'force-static'
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
