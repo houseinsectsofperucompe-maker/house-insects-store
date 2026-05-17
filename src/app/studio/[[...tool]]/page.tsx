f35cbd85a8dc6c330213500436e7e9389174d115
Housinsectsoffperu <housinsectsoffperu@192.168.18.23>
2026-05-17 11:16:17 -0500

    Fix Sanity Studio page

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,8 @@
-'use client'
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
 export const dynamic = 'force-static'
+export const revalidate = false
 
 export default function StudioPage() {
   return <NextStudio config={config} />
