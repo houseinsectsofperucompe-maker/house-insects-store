9812ff34df696e868bb23294f4208517e3a0fd3b
Housinsectsoffperu <housinsectsoffperu@192.168.18.23>
2026-05-18 08:28:38 -0500

    Fix Sanity project ID 4zd6pr6j

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'lyty7d3g',
+    projectId: '4zd6pr6j',
     dataset: 'production'
   },
   studioHost: 'houseinsectsofperu'
