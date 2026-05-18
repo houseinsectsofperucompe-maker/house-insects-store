eb5e722c4b3429f3935932b7f3043b0b5866a97b
Housinsectsoffperu <housinsectsoffperu@192.168.18.23>
2026-05-18 08:57:22 -0500

    Sanity HOUSE INSECTS OF PERU lyty7d3g

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '4zd6pr6j',
+    projectId: 'lyty7d3g',
     dataset: 'production'
   },
   studioHost: 'houseinsectsofperu'
