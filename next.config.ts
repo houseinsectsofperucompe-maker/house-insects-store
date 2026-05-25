23940a889e8c4be081a05f96cbad97c67d3ba522
Housinsectsoffperu <housinsectsoffperu@192.168.18.23>
2026-05-25 07:55:39 -0500

    fix: deshabilitar toolbar Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
+  devIndicators: false,
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/dv3mvukmq/**' }],
