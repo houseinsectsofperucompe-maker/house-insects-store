c5a7cb0688e1f5571f7b7f49612f5f1fa629bd16
Javier Zavala <houseinsectsperueirl@gmail.com>
2026-05-27 19:22:58 -0500

    fix: sanity client token directo para web

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -4,20 +4,22 @@ export const sanityClient = createClient({
   projectId: 'lyty7d3g',
   dataset: 'production',
   apiVersion: '2024-01-01',
-  useCdn: true,
+  token: 'sklfHrDIKRAM4FzHerAVfprMrhxYUfjuArRwMmhMRtgCb9Bwf6QfSqoLQ2kJwcj1s1awC86sXf6x7DdnZ',
+  useCdn: false,
 })
 
 export async function getEspecimenesSanity() {
   try {
-    const data = await sanityClient.fetch(`*[_type=="especie" && activo==true]{
+    const data = await sanityClient.fetch(`*[_type=="especie"]{
       "n": nombre,
       "p": precio,
       "s": stock,
-      "foto": fotoFrente.asset->url,
-      "fotoLado": fotoLado.asset->url,
-      "fotoReverso": fotoReverso.asset->url,
+      "foto": fotoFrente,
+      "fotoLado": fotoLado,
+      "fotoReverso": fotoReverso,
       familia,
-      video
+      video,
+      activo
     } | order(familia asc, nombre asc)`)
     return data
   } catch(e) {
