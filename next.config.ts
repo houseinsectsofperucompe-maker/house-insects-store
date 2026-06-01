4a9f8069ad14ff7cdf3359f76e6a3a089fcaeb83
Javier Zavala <houseinsectsperueirl@gmail.com>
2026-05-31 23:41:55 -0500

    fix: agregar Bunny.net a next.config remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
     formats: ['image/avif', 'image/webp'],
-    remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/dv3mvukmq/**' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/dv3mvukmq/**' },
+      { protocol: 'https', hostname: 'HouseInsects1967.b-cdn.net' },
+      { protocol: 'https', hostname: '*.b-cdn.net' },
+    ],
     minimumCacheTTL: 86400,
   },
 }
